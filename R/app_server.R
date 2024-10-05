a7ca8793060d2a90a53c580310d1175e67c773c7
EwokOzwok <eozmat@albany.edu>
2024-10-04 23:15:17 -0400

    Initial commit

---
+++ R/app_server.R
@@ -0,0 +1,132 @@
+#' The application server-side
+#'
+#' @param input,output,session Internal parameters for {shiny}.
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @import shinyMobile
+#' @import dplyr
+#' @import readxl
+#' @import DT
+#' @noRd
+app_server <- function(input, output, session) {
+
+  # Reactive expression to read the uploaded file
+  dataset <- reactive({
+    req(input$file)
+    # Read the uploaded file as a data frame
+    read_excel(input$file$datapath)
+  })
+
+  dataset_sort <- reactive({
+    req(input$file1)
+    # Read the uploaded file as a data frame
+    read_excel(input$file1$datapath)
+  })
+
+
+  # Reactive expression to filter the dataset based on user input
+  filtered_data <- reactive({
+    req(input$class_code, input$class_number)
+    df <- dataset()
+    df$CLASSNUMBER <- as.numeric(df$CLASSNUMBER)
+
+    class_numb <- as.numeric(input$class_number)
+    class_code <- input$class_code
+
+    # Filter the dataset
+    df_filtered <- df %>%
+      filter(CLASSCODE == class_code,
+             CLASSNUMBER == class_numb,
+             !grepl("Baseline Completed", Status))  # Exclude rows where 'Status' contains 'Baseline Completed'
+
+    df_filtered <- dplyr::select(df_filtered, `Email Address`, Link, Status, GROUP, CLASSCODE, CLASSNUMBER)
+
+    df_filtered
+  })
+
+
+
+  filtered_data1 <- reactive({
+    df <- dataset_sort()
+
+    # Sort the filtered dataset by SEND_DATE, earliest to latest
+    df_filtered <- df_filtered %>%
+      arrange(SEND_DATE)
+
+    df_filtered
+  })
+
+
+  # Preview the filtered dataset in the app using DT
+  output$preview <- renderDT({
+    req(filtered_data())
+
+    # Clean up potential NA values in the filtered data before displaying
+    filtered_clean <- filtered_data() %>%
+      na.omit()  # Remove rows with NA values if necessary
+
+    datatable(
+      filtered_clean,
+      options = list(pageLength = 10, autoWidth = TRUE),
+      class = 'cell-border stripe'  # Adds basic styling like cell borders and stripes
+    ) %>% formatStyle(
+      # Apply white text to all cells
+      columns = names(filtered_clean),
+      color = 'white',
+      backgroundColor = 'black',   # Set background color to black for contrast
+      fontWeight = 'bold'          # Make text bold
+    ) %>% formatStyle(
+      # Set table width to 100%
+      columns = 1:ncol(filtered_clean),
+      width = '100%'               # Automatically converted to 'width: 100%'
+    )
+  })
+
+
+
+  # Preview the filtered dataset in the app using DT
+  output$preview <- renderDT({
+    req(filtered_data1())
+
+    # Clean up potential NA values in the filtered data before displaying
+    filtered_clean <- filtered_data1()
+
+    datatable(
+      filtered_clean,
+      options = list(pageLength = 10, autoWidth = TRUE),
+      class = 'cell-border stripe'  # Adds basic styling like cell borders and stripes
+    ) %>% formatStyle(
+      # Apply white text to all cells
+      columns = names(filtered_clean),
+      color = 'white',
+      backgroundColor = 'black',   # Set background color to black for contrast
+      fontWeight = 'bold'          # Make text bold
+    ) %>% formatStyle(
+      # Set table width to 100%
+      columns = 1:ncol(filtered_clean),
+      width = '100%'               # Automatically converted to 'width: 100%'
+    )
+  })
+
+
+
+  # Generate a CSV for download
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste("CLASSROOM NON-COMPLETERS -- ", "CLASS --", input$class_code, " ", input$class_number, " - Downloaded on - ", Sys.Date(), ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(filtered_data(), file, row.names = FALSE)
+    }
+  )
+
+  output$downloadData1 <- downloadHandler(
+    filename = function() {
+      paste("Gift Card Data Sorted -- ", input$timepoint, " - Downloaded on - ", Sys.Date(), ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(filtered_data1(), file, row.names = FALSE)
+    }
+  )
+
+}
---
+++ R/app_ui.R
@@ -0,0 +1,133 @@
+#' The application User-Interface
+#'
+#' @param request Internal parameter for `{shiny}`.
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @import shinyMobile
+#' @import htmltools
+#' @import DT
+#' @noRd
+app_ui <- function(request) {
+  tagList(
+    # Leave this function for adding external resources
+    golem_add_external_resources(),
+    # Your application UI logic
+
+    shinyMobile::f7Page(
+      title = "Survey Manager",
+      options = list(theme=c("auto"), dark=TRUE, preloader = F,  pullToRefresh=F),
+      allowPWA=TRUE,
+
+
+      f7TabLayout(
+        # panels are not mandatory. These are similar to sidebars
+        navbar = f7Navbar(
+          title= "Survey Manager"),
+
+
+        # f7Tabs is a special toolbar with included navigation
+        f7Tabs(
+          animated = TRUE,
+          id = "tabs",
+          swipeable = F,
+          f7Tab(
+            tabName = "WelcomeTab",
+            icon = f7Icon("envelope_fill"),
+            active = TRUE,
+            hidden= F,
+            f7Block(
+              f7Shadow(
+                intensity = 5,
+                hover = TRUE,
+                f7Card(
+                  f7Align(h2("Step 1: Upload the Baseline List from GDrive"),side=c("left")),
+                  f7File("file", "Upload XLSX File", multiple = F, accept = ".xlsx"),
+                  br(),
+                  f7Align(h2("Step 2: Enter the Class Code and Class Number"),side=c("left")),
+                  f7Text("class_code", "Enter Class Code", placeholder = "ECPY"),
+                  br(),
+                  f7Text("class_number", "Enter Class Number", placeholder = "101"),
+                  hr(),
+                  br(),
+                  f7Align(h2("Step 3: Extract Non-Completers for Mail Merge"),side=c("left")),
+                  br(),
+                  f7Button("generate", "Generate Mail Merge List"),
+                  br(),
+                  f7DownloadButton("downloadData", "Download Filtered CSV"),
+                  br(),
+                  br(),
+                  DTOutput("preview"),
+                  footer = NULL,
+                  hairlines = F, strong = T, inset = F, tablet = FALSE)
+              )
+            )
+          ),
+
+          f7Tab(
+            tabName = "SortTab",
+            icon = f7Icon("sort_up"),
+            active = F,
+            hidden= F,
+            f7Block(
+              f7Shadow(
+                intensity = 5,
+                hover = TRUE,
+                f7Card(
+                  f7Align(h2("Step 1: Upload the Exit or Follow-up List from GDrive"),side=c("left")),
+                  f7File("file1", "Upload XLSX File", multiple = F, accept = ".xlsx"),
+                  br(),
+                  f7Align(h2("Step 2: Sort by Gift Card Send Date"),side=c("left")),
+                  br(),
+                  f7Button("sort", "Sort"),
+                  hr(),
+                  br(),
+                  f7Select("timepoint", "Select the time-point", choice = c("Exit", "Follow-up", "6month Follow-up")),
+                  br(),
+                  f7DownloadButton("downloadData1", "Download Filtered CSV"),
+                  br(),
+                  br(),
+                  DTOutput("preview"),
+                  footer = NULL,
+                  hairlines = F, strong = T, inset = F, tablet = FALSE)
+              )
+            )
+          )
+        )
+      )
+    )
+  )
+}
+
+#' Add external Resources to the Application
+#'
+#' This function is internally used to add external
+#' resources inside the Shiny application.
+#'
+#' @import shiny
+#' @importFrom golem add_resource_path activate_js favicon bundle_resources
+#' @noRd
+golem_add_external_resources <- function() {
+  # add_resource_path(
+  #   "www",
+  #   app_sys("./www"),
+  # )
+
+  tags$head(
+
+    # favicon -----------------------------------------------------------------
+    favicon(),
+    # bundle_resources(
+    #   path = app_sys("./www"),
+    #   app_title = "MHScreener"),
+    # includeCSS("./www/newcss.css"),
+
+    HTML('<link rel="stylesheet" type="text/css" href="https://ewokozwok.github.io/MHScreener/www/framework7.bundle.min.css">')
+
+
+
+    # Add here other external resources
+    # for example, you can add shinyalert::useShinyalert()
+
+  )
+
+}
